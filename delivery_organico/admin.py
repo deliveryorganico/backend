9f1bb95c1876391539656ca7d99c607efeac4f50
Juan Cruz Mare <jcmare18@gmail.com>
2018-10-13 14:56:14 -0300

    cambios settings, admin

--- delivery_organico/admin.py
+++ delivery_organico/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Address)
+admin.site.register(City)
+admin.site.register(Locality)
+admin.site.register(Company)
+admin.site.register(Branch)
+admin.site.register(Product)
+admin.site.register(Favorite)
