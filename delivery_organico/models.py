7bd865273bbbf8a6f2bc93f995d2b1f5496f895d
tinchoolivari <martin.olivari01@gmail.com>
2018-11-09 09:58:56 -0300

    profile pertenece a address

--- delivery_organico/models.py
+++ delivery_organico/models.py
@@ -2,14 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-
 # Create your models here.
 
 class City(models.Model):
     ''' Foreign Keys '''
 
     ''' Atributes '''
-    CITIES=(
+    CITIES = (
         ('CBA', 'Cordoba'),
         ('MDZ', 'Mendoza'),
     )
@@ -23,7 +22,7 @@ class Locality(models.Model):
     ''' Foreign Keys '''
 
     ''' Atributes '''
-    LOCALITIES=(
+    LOCALITIES = (
         ('CBA', 'Cordoba'),
         ('MDZ', 'Mendoza'),
     )
@@ -48,6 +47,8 @@ class Address(models.Model):
     ''' Foreign Keys '''
     city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='fk_cityAddress')
     locality = models.ForeignKey(Locality, on_delete=models.CASCADE, related_name='fk_localityAddress')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='fk_profileAddress')
+
     ''' Atributes '''
     address = models.CharField(max_length=60)
     alias = models.CharField(max_length=40)
@@ -62,25 +63,24 @@ class Address(models.Model):
 
 class Company(models.Model):
     ''' Foreign Keys '''
-    address=models.ForeignKey(Address, on_delete=models.CASCADE, related_name='fk_addressCompany')
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='fk_addressCompany')
 
     '''Atributes'''
-    name=models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
 
     '''Functions'''
     def __str__(self):
         return '{}'.format(self.name)
 
-
 class Branch(models.Model):
     '''Foreign Keys'''
-    address=models.ForeignKey(Address, on_delete=models.CASCADE, related_name='fk_addressBranch')
-    company=models.ForeignKey(Company, on_delete=models.CASCADE, related_name='fk_companyBranch')
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='fk_addressBranch')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='fk_companyBranch')
 
     '''Atributes'''
-    reputation=models.FloatField(validators=[MaxValueValidator(5.0), MinValueValidator(0.0)])
-    n_phone=models.CharField(max_length=30)
-    name=models.CharField(max_length=30)
+    reputation = models.FloatField(validators=[MaxValueValidator(5.0), MinValueValidator(0.0)])
+    n_phone = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
 
     '''Functions'''
     def __str__(self):
@@ -100,11 +100,10 @@ class Product(models.Model):
     def __str__(self):
         return '{}, {}'.format(self.title, self.price)
 
-
 class Favorite(models.Model):
     ''' Foreign Keys '''
-    profile=models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='fk_profileFavorite')
-    branch=models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='fk_branchFavorite')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='fk_profileFavorite')
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='fk_branchFavorite')
 
     '''Functions'''
     def __str__(self):
